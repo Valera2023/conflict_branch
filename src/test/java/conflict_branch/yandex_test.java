aa142dd6e9f884a0e3e8861ca2707b05a317c0bc
Valerii <valera-asratyan90@yandex.ru>
2023-08-06 19:30:48 +0300

    new test ya

--- src/test/java/conflict_branch/yandex_test.java
+++ src/test/java/conflict_branch/yandex_test.java
@@ -3,8 +3,11 @@ package conflict_branch;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class yandex_test {
     void yandexTest(){
-        
+        sleep(50000);
+
     }
 }
